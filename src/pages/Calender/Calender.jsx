79e4fd8a37c5cc9e0494cc30cad6a23fe2f3c63e
Aditya <akaditya394@gmail.com>
2022-10-18 20:38:20 +0530

    availability updated

--- src/pages/Calender/Calender.jsx
+++ src/pages/Calender/Calender.jsx
@@ -1,16 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import PhnSidebar from "../../components/PhnSidebar/PhnSidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Search from "../../components/search/Search";
 import styles from "./Calender.module.css";
+import { doc, setDoc } from "firebase/firestore";
+import { db, getMentorFromDatabase } from "../../firebase/firebase";
+import { updateUserInDatabse } from "../../firebase/firebase";
 
 const Calender = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const [fetchedMentor, setFetchedMentor] = useState("");
+  var availabilityArray = ["0", "0", "0", "0", "0", "0", "0"];
+  // const [availabilityTime, setAvailabilityTime] = useState({
+  //   availableFrom: "09:00",
+  //   availableTill: "17:00",
+  // });
+
+  // const selectStartTime = (e) => {
+  //   setAvailabilityTime((prevState) => {
+  //     return { ...prevState, availableFrom: e.target.value };
+  //   });
+  //   console.log(availabilityTime);
+  // };
 
   const updateWidth = () => {
     setWidth(window.innerWidth);
   };
+
+  const week = [
+    {
+      id: 0,
+      day: "Sunday",
+    },
+    { id: 1, day: "Monday" },
+    { id: 2, day: "Tuesday" },
+    { id: 3, day: "Wednesday" },
+    { id: 4, day: "Thursday" },
+    { id: 5, day: "Friday" },
+    { id: 6, day: "Saturday" },
+  ];
+
+  const getMentor = useCallback(async () => {
+    const results = await getMentorFromDatabase("jatin.dsquare@gmail.com");
+    setFetchedMentor(results);
+  }, []);
+
+  const updateAvailability = async () => {
+    await updateUserInDatabse(fetchedMentor.email, "Users", {
+      ...fetchedMentor,
+      availability: availabilityArray,
+    });
+  };
+
+  const updateDayToArray = (id) => {
+    availabilityArray[id] = "1";
+    console.log(availabilityArray);
+  };
+
+  useEffect(() => {
+    getMentor();
+  }, []);
+
+  console.log(fetchedMentor);
 
   useEffect(() => {
     window.addEventListener("resize", updateWidth);
@@ -34,26 +86,33 @@ const Calender = () => {
               <h3 className={styles.text}>Available Hours</h3>
               <div className={styles.hours}>
                 <select name="hours" id="" className={styles.starthr}>
-                  <option value="">9:00 am</option>
-                  <option value="">10:00 am</option>
-                  <option value="">12:00 am</option>
+                  <option value="">09:00</option>
+                  <option value="">10:00</option>
+                  <option value="">12:00</option>
                 </select>
                 <select name="hours" id="" className={styles.endhr}>
-                  <option value="">5:00 pm</option>
-                  <option value="">6:00 pm</option>
-                  <option value="">7:00 pm</option>
+                  <option value="">17:00</option>
+                  <option value="">18:00</option>
+                  <option value="">19:00</option>
                 </select>
               </div>
               <h3 className={styles.text}>Available days</h3>
               <div className={styles.days}>
-                <button>Sunday</button>
-                <button>Monday</button>
-                <button>Tuesday</button>
-                <button>Wednesday</button>
-                <button>Thursday</button>
-                <button>Friday</button>
-                <button>Saturday</button>
+                {week.map((weekday) => (
+                  <button
+                    onClick={() => updateDayToArray(weekday.id)}
+                    key={weekday.id + Math.random()}
+                  >
+                    {weekday.day}
+                  </button>
+                ))}
               </div>
+              <button
+                onClick={updateAvailability}
+                className={styles.confirmBtn}
+              >
+                Confirm
+              </button>
               <p className={styles.info}>
                 Don’t worry! You’ll be able to further customize your
                 availability later on.
